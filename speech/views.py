fd21b07871e4cc8f0c492a1158407b483dd66b18
17issJLW <32334181+17issJLW@users.noreply.github.com>
2019-06-27 20:00:55 +0800

    语言识别

--- speech/views.py
+++ speech/views.py
@@ -10,16 +10,13 @@ class SpeechRecognition(APIView):
     def post(self,request):
         request_data = request.data
         file = request_data.get("file")
-        file2 = request_data.get("file2")
-        print(file2)
         print(file)
         with open("temp.wav","wb") as f:
             f.write(file.read())
-
-        try:
-            res = audio_recognition("temp.wav")
-        except:
-            return Response({"error":"Could not understand audio"},status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        # try:
+        res = audio_recognition("temp.wav")
+        # except:
+        #     return Response({"error":"Could not understand audio"},status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
         return Response({"result":res},status=status.HTTP_200_OK)
 
