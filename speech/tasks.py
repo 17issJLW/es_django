1a90c966935e79ab09ca377aca0f32beb534f2ed
17issJLW <32334181+17issJLW@users.noreply.github.com>
2019-06-27 15:18:11 +0800

    语言识别

---
+++ speech/tasks.py
@@ -0,0 +1,9 @@
+import speech_recognition as sr
+
+
+def audio_recognition(audio_file):
+    r = sr.Recognizer()
+    with sr.AudioFile(audio_file) as source:
+        r.adjust_for_ambient_noise(source,duration=1)
+        audio = r.record(source)
+        res = r.recognize_sphinx(audio).replace(" ",'')
---
+++ speech/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .tasks import *
+
+# Create your views here.
+class SpeechRecognition(APIView):
+
+    def post(self,request):
+        request_data = request.data
+        file = request.get("file")
+        with open("temp.wav","wb") as f:
+            f.write(file)
+
+        try:
+            res = audio_recognition("temp.wav")
+        except:
+            return Response({"error":"Could not understand audio"},status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+        return Response({"result":res},status=status.HTTP_200_OK)
+
+
+# class Spider(APIView):
+#
+#     def get(self,request):
+
