26384f41360b1ecd96064c00b30c1ca8f7a52bfc
17issJLW <32334181+17issJLW@users.noreply.github.com>
2019-07-08 17:17:05 +0800

    修改url

--- esspider/urls.py
+++ esspider/urls.py
@@ -4,6 +4,8 @@ from .views import *
 
 urlpatterns = [
     path("account_manage/", AccountManage.as_view(), name="AccountManage"),
+    path("account_manage/<id>", AccountManage.as_view(), name="AccountManage"),
     # path("<uid>", AdminView.as_view(), name="AdminView"),
     path("doc_data_manage/", DocDataView.as_view(),name="DocDataView"),
+    path("doc_data_manage/<id>", DocDataView.as_view(),name="DocDataView"),
 ]
